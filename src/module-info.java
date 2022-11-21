a94bc9282dd9a65b72544216e7934ca7896bcc66
Mahesh <gmaheshwar317@gmail.com>
2022-11-21 13:34:56 +0530

    alpha commit 1

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mahes
+ *
+ */
+module Alpha1 {
+}
---
+++ src/package1/code.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class code {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alpha project");
+
+	}
+
+}
